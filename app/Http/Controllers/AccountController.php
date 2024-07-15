6f912955468925da66e1b1be830dde1a8486ed80
ngo_huu_phong@management-partners.co.jp <ngohuuphong.147@gmail.com>
2024-07-16 00:08:00 +0700

    update controller

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -7,4 +7,23 @@ use Illuminate\Http\Request;
 class AccountController extends Controller
 {
     //
+   public function login(Request $request)
+    {
+        return view('account.login');
+    }
+
+    public function register(Request $request)
+    {
+        return view('account.register');
+    }
+
+    public function resetPassword(Request $request)
+    {
+        return view('account.resetPassword');
+    }
+
+    public function logout(Request $request)
+    {
+        return view('account.login');
+    }
 }
--- app/Http/Controllers/CEOController.php
+++ app/Http/Controllers/CEOController.php
@@ -7,4 +7,29 @@ use Illuminate\Http\Request;
 class CEOController extends Controller
 {
     //
+    public function dashboard(Request $request)
+    {
+        return view('ceo.dashboard');
+    }
+
+    public function myPage(Request $request)
+    {
+        return view('ceo.myPage');
+    }
+
+    public function idealList(Request $request)
+    {
+        return view('ceo.idealList');
+    }
+
+    public function ideal(Request $request)
+    {
+        return view('ceo.ideal', ['id' => $request->id]);
+    }
+
+    public function report(Request $request)
+    {
+        return view('ceo.report');
+    }
+    
 }
