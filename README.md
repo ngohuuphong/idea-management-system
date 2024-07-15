742651506a94cf747513a1156e29d2707f7ea804
ngo_huu_phong@management-partners.co.jp <ngo_huu_phong@management-partners.co.jp>
2024-07-15 23:05:08 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# idea-management-system
+ 
---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\CEOController;
+use App\Http\Controllers\ManagerController;
+use App\Http\Controllers\MemberController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::group(['prefix' => 'member', 'as' => 'member.'], function () {
+    Route::controller(MemberController::class)->group(function () {
+        Route::get('/index', 'index')->name('index');
+        Route::get('/mypage', 'myPage')->name('myPage');
+        Route::get('/post/{id}', 'post')->name('post')->where('id', '[0-9]+');
+        Route::get('/post/new', 'newPost')->name('newPost');
+    });
+});
+
+Route::group(['prefix' => 'manager', 'as' => 'manager.'], function () {
+    Route::controller(ManagerController::class)->group(function () {
+        Route::get('/index', 'index')->name('index');
+        Route::get('/mypage', 'myPage')->name('myPage');
+        Route::get('/post/{id}', 'post')->name('post')->where('id', '[0-9]+');
+    });
+});
+
+Route::group(['prefix' => 'ceo', 'as' => 'ceo.'], function () {
+    Route::controller(CEOController::class)->group(function () {
+        Route::get('/dashboard', 'dashboard')->name('dashboard');
+        Route::get('/mypage', 'myPage')->name('myPage');
+        Route::get('/ideal', 'ideal')->name('ideal');
+        Route::get('/ideal/{id}', 'ideal')->name('ideal')->where('id', '[0-9]+');
+        Route::get('/report', 'report')->name('report');
+    });
+});
+
+Route::group(['prefix' => 'account', 'as' => 'account.'], function () {
+    Route::controller(AccountController::class)->group(function () {
+        Route::get('/login', 'login')->name('login');
+        Route::get('/password-reset', 'passwordReset')->name('password-reset');
+        Route::get('/signup', 'signup')->name('signup');
+        Route::get('/logout', 'logout')->name('logout');
+    });
+});
+
